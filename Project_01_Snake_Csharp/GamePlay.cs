b7fec1a03e624943246e2da235ddb092539287b4
Sergey Boykov <boykov.sergei@gmail.com>
2022-04-05 16:06:01 +0500

    Class GamePlay created

---
+++ Project_01_Snake_Csharp/GamePlay.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Project_01_Snake_Csharp.Enums;
+using Project_01_Snake_Csharp.Factory;
+using Project_01_Snake_Csharp.Helpers;
+using Project_01_Snake_Csharp.Installers;
+using Project_01_Snake_Csharp.Lines;
+
+namespace Project_01_Snake_Csharp
+{
+    public class GamePlay
+    {
+        public void StartGame()
+        {
+            int score = 0;
+
+            LineInstaller line = new LineInstaller();
+            line.DrawShape();
+
+            Point food = FoodFactory.GetRandomFood(119, 20, '+');
+            Console.ForegroundColor = ColorHelper.GetRandomColor(new Random().Next(1, 5));
+            food.DrawPoint();
+            Console.ForegroundColor = ColorHelper.ResetColor();
+            //Console.ResetColor(); // как вариант
+
+            Snake snake = new Snake();
+            snake.CreateSnake(5, new Point(5, 5, 'Z'), DirectionEnum.Right);
+            snake.DrawLine();
+
+            ScoreHelper.GetScore(score);
+
+            while (true)
+            {
+                if (line.Collision(snake) || snake.CollisionWithOwnTail())
+                {
+                    break;
+                }
+
+                if (snake.Eat(food))
+                {
+                    score++;
+                    ScoreHelper.GetScore(score);
+
+                    food = FoodFactory.GetRandomFood(119, 20, '+');
+                    Console.ForegroundColor = ColorHelper.GetRandomColor(new Random().Next(1, 5));
+                    food.DrawPoint();
+                    Console.ForegroundColor = ColorHelper.ResetColor();
+                }
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.PressKey(key.Key);
+                }
+
+                snake.Move();
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
--- Project_01_Snake_Csharp/Program.cs
+++ Project_01_Snake_Csharp/Program.cs
@@ -12,50 +12,8 @@ namespace Project_01_Snake_Csharp
     {
         static void Main(string[] args)
         {
-            int score = 0;
-            
-            LineInstaller line = new LineInstaller();
-            line.DrawShape();
-
-            Point food = FoodFactory.GetRandomFood(119, 20, '+');
-            Console.ForegroundColor = ColorHelper.GetRandomColor(new Random().Next(1, 5));
-            food.DrawPoint();
-            Console.ForegroundColor = ColorHelper.ResetColor();
-            //Console.ResetColor(); // как вариант
-
-            Snake snake = new Snake();
-            snake.CreateSnake(5, new Point(5, 5, 'Z'), DirectionEnum.Right);
-            snake.DrawLine();
-
-            ScoreHelper.GetScore(score);
-
-            while (true)
-            {
-                if (line.Collision(snake) || snake.CollisionWithOwnTail())
-                {
-                    break;
-                }
-                
-                if (snake.Eat(food))
-                {
-                    score++;
-                    ScoreHelper.GetScore(score);
-
-                    food = FoodFactory.GetRandomFood(119, 20, '+');
-                    Console.ForegroundColor = ColorHelper.GetRandomColor(new Random().Next(1, 5));
-                    food.DrawPoint();
-                    Console.ForegroundColor = ColorHelper.ResetColor();
-                }
-                
-                if (Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo key = Console.ReadKey();
-                    snake.PressKey(key.Key);
-                }
-
-                snake.Move();
-                Thread.Sleep(200);
-            }
+            GamePlay gamePlay = new GamePlay();
+            gamePlay.StartGame();
 
             Console.WriteLine("Game Over...");
         }
